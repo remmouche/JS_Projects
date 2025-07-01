014af045c05fa17b12fe86dad94c85398df5e8cf
remmouche <121981104+remmouche@users.noreply.github.com>
2025-07-01 17:23:22 +0000

    add hidden search project

---
+++ hidden_search/script.js
@@ -0,0 +1,11 @@
+// script
+
+const search = document.querySelector('.search');
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn')
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active');
+    input.focus()
+})
+
